dd8f9df3b74fc98fa528326c5f20e0d194218e1a
Tonire <tonire.rebollo@gmail.com>
2022-12-16 21:11:41 +0100

    Aoc5 part2 wip

--- aoc5/src/main.rs
+++ aoc5/src/main.rs
@@ -1,4 +1,79 @@
-use std::fs;
+use std::{fs, ops::Range};
+
+fn part1(movements_vector: &Vec<(i32, i32, i32)>)
+{
+    let mut stacks: Vec<Vec<char>> = vec![
+        ['D', 'L', 'J', 'R', 'V', 'G', 'F'].to_vec(), 
+        ['T', 'P', 'M', 'B', 'V', 'H', 'J', 'S'].to_vec(),
+        ['V', 'H', 'M', 'F', 'D', 'G', 'P', 'C'].to_vec(),
+        ['M', 'D', 'P', 'N', 'G', 'Q'].to_vec(),
+        ['J', 'L', 'H', 'N', 'F'].to_vec(),
+        ['N', 'F', 'V', 'Q', 'D', 'G', 'T', 'Z'].to_vec(),
+        ['F', 'D', 'B', 'L'].to_vec(),
+        ['M', 'J', 'B', 'S', 'V', 'D', 'N'].to_vec(),
+        ['G', 'L', 'D'].to_vec()
+    ];
+    
+    for current_move in movements_vector
+    {
+        for _ in 0..current_move.0
+        {
+            if let Some (removed) = stacks[(current_move.1 - 1) as usize].pop()
+            {
+                stacks[(current_move.2 - 1) as usize].push(removed);
+            }
+        }
+    }
+
+    for stack in stacks
+    {
+        if stack.len() != 0
+        {
+            print!("{}", stack.iter().last().unwrap());
+        }
+    }
+    println!("\n");
+}
+
+fn part2(movements_vector: &Vec<(i32, i32, i32)>)
+{
+    let mut stacks: Vec<Vec<char>> = vec![
+        ['D', 'L', 'J', 'R', 'V', 'G', 'F'].to_vec(), 
+        ['T', 'P', 'M', 'B', 'V', 'H', 'J', 'S'].to_vec(),
+        ['V', 'H', 'M', 'F', 'D', 'G', 'P', 'C'].to_vec(),
+        ['M', 'D', 'P', 'N', 'G', 'Q'].to_vec(),
+        ['J', 'L', 'H', 'N', 'F'].to_vec(),
+        ['N', 'F', 'V', 'Q', 'D', 'G', 'T', 'Z'].to_vec(),
+        ['F', 'D', 'B', 'L'].to_vec(),
+        ['M', 'J', 'B', 'S', 'V', 'D', 'N'].to_vec(),
+        ['G', 'L', 'D'].to_vec()
+    ];
+    
+    for current_move in movements_vector
+    {
+        let split_location: usize = (current_move.0 - 1) as usize;
+        let from_stack: &Vec<char> = &mut stacks[(current_move.1 - 1) as usize];
+        
+        let mut new_vec: Vec<char> = Vec::new();
+        for current in split_location..from_stack.len() - 1
+        {
+            new_vec.push(from_stack[current]);
+        }
+        let to_stack = &mut stacks[(current_move.2 - 1) as usize];
+        to_stack.append(&mut new_vec);
+
+        //from_stack.cut
+    }
+
+    for stack in stacks
+    {
+        if stack.len() != 0
+        {
+            print!("{}", stack.iter().last().unwrap());
+        }
+    }
+    println!("\n");
+}
 
 fn main()
 {
@@ -20,7 +95,7 @@ fn main()
     // [N] [C]    
     // [Z] [M] [P]
     //  1   2   3 
-    let mut stacks: Vec<Vec<char>> = vec![['Z', 'N'].to_vec(), ['M', 'C', 'D'].to_vec(), ['P'].to_vec()];
+    //let mut stacks: Vec<Vec<char>> = vec![['Z', 'N'].to_vec(), ['M', 'C', 'D'].to_vec(), ['P'].to_vec()];
 
     //     [S] [C]         [Z]            
     // [F] [J] [P]         [T]     [N]    
@@ -32,35 +107,7 @@ fn main()
     // [D] [T] [V] [M] [J] [N] [F] [M] [G]
     //  1   2   3   4   5   6   7   8   9 
 
-    let mut stacks: Vec<Vec<char>> = vec![
-        ['D', 'L', 'J', 'R', 'V', 'G', 'F'].to_vec(), 
-        ['T', 'P', 'M', 'B', 'V', 'H', 'J', 'S'].to_vec(),
-        ['V', 'H', 'M', 'F', 'D', 'G', 'P', 'C'].to_vec(),
-        ['M', 'D', 'P', 'N', 'G', 'Q'].to_vec(),
-        ['J', 'L', 'H', 'N', 'F'].to_vec(),
-        ['N', 'F', 'V', 'Q', 'D', 'G', 'T', 'Z'].to_vec(),
-        ['F', 'D', 'B', 'L'].to_vec(),
-        ['M', 'J', 'B', 'S', 'V', 'D', 'N'].to_vec(),
-        ['G', 'L', 'D'].to_vec()
-        ];
-    
-
-    for current_move in movements_vector
-    {
-        for _ in 0..current_move.0
-        {
-            if let Some (removed) = stacks[(current_move.1 - 1) as usize].pop()
-            {
-                stacks[(current_move.2 - 1) as usize].push(removed);
-            }
-        }
-    }
-    for stack in stacks
-    {
-        if stack.len() != 0
-        {
-            print!("{}", stack.iter().last().unwrap());
-        }
-    }
+    part1(&movements_vector);
+    part2(&movements_vector);
 
 }
